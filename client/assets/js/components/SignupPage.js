800c38e29562573f808dd09bd76866bde2e67572
T_Kenta <tamkchi.fugu@gmail.com>
2018-01-23 16:33:06 +0900

    ログイン、サインアップ完成

--- client/assets/js/components/SignupPage.js
+++ client/assets/js/components/SignupPage.js
@@ -1,5 +1,5 @@
 import React ,{ Component }from 'react'
-import {Grid,Col} from "react-bootstrap"
+import {Row,Col} from "react-bootstrap"
 import Paper from 'material-ui/Paper';
 import SignupForm from '../containers/SignupForm'
 
@@ -8,21 +8,23 @@ class SignupPage extends Component{
     const style = {
       width: '100%',
       padding: 30,
-      marginTop:100,
+
 
       position: 'absolute',
       display: 'inline-block',
     };
     return(
       <div className="log">
-        <Grid>
-        <Col smOffset={3} sm={6}>
-          <Paper style = {style} className="login_up" zDepth={3}>
+        <Row>
+        <Col xs={11} smOffset={3} sm={6}>
+          <div className="topmargin">
+          <Paper style = {style} zDepth={3}>
             <h4>アカウント作成</h4>
             <SignupForm/>
           </Paper>
+          </div>
         </Col>
-        </Grid>
+        </Row>
       </div>
     )
   }
