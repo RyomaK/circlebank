d6eeaeb8470a707c640f62a358d4f7b45e99b4b8
T_Kenta <tamkchi.fugu@gmail.com>
2018-01-27 17:15:45 +0900

    テスト

--- client/assets/js/components/Circle.js
+++ client/assets/js/components/Circle.js
@@ -45,7 +45,7 @@ const circle = this.props.circle
       <Tabs
           value={this.state.value}
           onChange={this.handleChange.bind(this)}
-         className="fontChange1 whitePage">
+         className="fontChange1 whitePage pad">
           <Tab label="団体紹介" value="a">
             <div className="centerPosition whitePage">
               <div className="circleInt">
