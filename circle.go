c20092c37c3c45671d1b008f27ba1257c35134a0
ryomak <ryooomaaa0413@outlook.com>
2018-02-09 12:57:30 +0900

    rm

--- circle.go
+++ circle.go
@@ -92,7 +92,6 @@ func (s *Server) Route(addr string) {
 	a.Path("/user/upload").HandlerFunc(users.UploadPicture).Methods("POST")
 
 	//subrouter-Admin
-	//to-do adminだけしかadmin/には入れないようにする
 	admin := mux.NewRouter()
 	r.PathPrefix("/admin").Handler(negroni.New(
 		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
