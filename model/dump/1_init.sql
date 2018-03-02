9eadbe1ccae7a92eaeab8ada91a01d8af61b1cfb
ryomak <ryooomaaa0413@outlook.com>
2018-03-03 01:50:23 +0900

    migration

--- model/dump/1_init.sql
+++ model/dump/1_init.sql
@@ -7,7 +7,7 @@
 #
 # ホスト: 127.0.0.1 (MySQL 5.7.19)
 # データベース: circle_bank
-# 作成時刻: 2018-02-20 05:35:08 +0000
+# 作成時刻: 2018-03-02 16:49:12 +0000
 # ************************************************************
 
 
@@ -22,6 +22,8 @@
 
 # テーブルのダンプ circles
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `circles`;
 
 CREATE TABLE `circles` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -39,10 +41,25 @@ CREATE TABLE `circles` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `circles` WRITE;
+/*!40000 ALTER TABLE `circles` DISABLE KEYS */;
+
+INSERT INTO `circles` (`id`, `univ_id`, `name`, `url_name`, `image`, `number`, `gender_ratio`, `introduction`, `excite`, `fee`, `campus`, `message_for_fresh`)
+VALUES
+	(1,1,'hands up','handsup','img/circles/1.png',30,'1:1','アットホームなバスケットボールサークルです',3,5000,'京田辺','いつでもきてね'),
+	(2,1,'fly speck','flyspeck','img/circles/2.png',100,'1:1','テニス',9,3000,'京田辺/今出川','いつでもきてねえええ'),
+	(3,1,'Nexus','nexus','img/circles/3.png',100,'1:1','軽音',2,12000,'京田辺/今出川','軽音サークルNexusです。\n気軽に見に来てください。'),
+	(4,1,'平成企画合戦ピテクス','pitex','img/circles/4.png',50,'1:1','企画サークル',1,2000,'京田辺/今出川','へんなことたくさんしてます。'),
+	(5,1,'軽音サークル','music','img/circles/5.png',100,'10:0','軽音',1,500000,'今出川','まじめに軽音してます。');
+
+/*!40000 ALTER TABLE `circles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # テーブルのダンプ circles_tags
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `circles_tags`;
 
 CREATE TABLE `circles_tags` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -51,24 +68,28 @@ CREATE TABLE `circles_tags` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `circles_tags` WRITE;
+/*!40000 ALTER TABLE `circles_tags` DISABLE KEYS */;
 
+INSERT INTO `circles_tags` (`id`, `circle_id`, `tag_id`)
+VALUES
+	(1,1,1),
+	(2,1,2),
+	(3,2,3),
+	(4,1,3),
+	(5,2,4),
+	(6,5,7),
+	(7,4,5),
+	(8,2,6);
 
-# テーブルのダンプ comments
-# ------------------------------------------------------------
-
-CREATE TABLE `comments` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `circle_id` int(11) DEFAULT NULL,
-  `point` int(11) NOT NULL,
-  `text` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+/*!40000 ALTER TABLE `circles_tags` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # テーブルのダンプ delegetes
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `delegetes`;
 
 CREATE TABLE `delegetes` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -78,10 +99,25 @@ CREATE TABLE `delegetes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `delegetes` WRITE;
+/*!40000 ALTER TABLE `delegetes` DISABLE KEYS */;
+
+INSERT INTO `delegetes` (`id`, `circle_id`, `name`, `contact`)
+VALUES
+	(1,1,'田中','tanaka@mail.com'),
+	(2,2,'大野','arashi@mail.com'),
+	(3,3,'谷','sample@sample'),
+	(4,4,'重松','sample@sample'),
+	(5,5,'田中','sample@sample');
+
+/*!40000 ALTER TABLE `delegetes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # テーブルのダンプ events
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `events`;
 
 CREATE TABLE `events` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -96,34 +132,30 @@ CREATE TABLE `events` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `events` WRITE;
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
 
+INSERT INTO `events` (`id`, `circle_id`, `name`, `image`, `agenda`, `place`, `detail`, `capacity`, `fee`)
+VALUES
+	(1,2,'ほわ音でー','img/users/default.png','2017-02-14 15:41:30','a','f',10,10),
+	(2,1,'ホワイトデー','img/users/default.png','2017-02-15 00:00:00','fsad','fds',0,0),
+	(3,2,'fsa','img/users/default.png','2017-02-14 00:00:00','fsa','fdsa',1,1),
+	(4,1,'水族館デー','img/users/default.png','2018-04-06 00:00:00','京都水族館','京田辺駅集合です',100,1000),
+	(5,2,'宅ぱ','img/users/default.png','2018-04-04 00:00:00','会長の家','鍋をやります',100,0),
+	(6,3,'お花見','img/users/default.png','2018-03-04 00:00:00','円山公園','みんなで桜見ながらご飯食べましょう',1000,100),
+	(7,4,'お絵かき','img/users/default.png','2018-04-05 00:00:00','ローム3F','一番面白い絵かけた人の勝ちです',100,0),
+	(8,5,'新歓ライブ','img/users/default.png','2018-04-29 00:00:00','寒梅館','9:00からライブします。無料です',100,1000),
+	(9,4,'新歓合宿','img/users/default.png','2018-05-04 00:00:00','天橋立','合宿いきます',50,12000),
+	(10,3,'新歓ライブ','img/users/default.png','2018-04-05 00:00:00','京都246','入場無料です。ぜひきてください',10000,500);
 
-# テーブルのダンプ events_schedules
-# ------------------------------------------------------------
-
-CREATE TABLE `events_schedules` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `event_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# テーブルのダンプ likes
-# ------------------------------------------------------------
-
-CREATE TABLE `likes` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `circle_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # テーブルのダンプ tags
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tags`;
 
 CREATE TABLE `tags` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -132,37 +164,59 @@ CREATE TABLE `tags` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
 
+INSERT INTO `tags` (`id`, `name`, `class_name`)
+VALUES
+	(1,'バスケットボール','運動'),
+	(2,'アットホーム','その他'),
+	(3,'京田辺','その他'),
+	(4,'今出川','その他'),
+	(5,'飲みサークル','その他'),
+	(6,'テニス','運動'),
+	(7,'軽音','文化'),
+	(8,'野球','運動'),
+	(9,'女の子可愛い','その他');
 
-# テーブルのダンプ universities
-# ------------------------------------------------------------
-
-CREATE TABLE `universities` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `url_name` varchar(255) DEFAULT NULL,
-  `name` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # テーブルのダンプ users
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `univ_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
-  `gender` char(2) NOT NULL DEFAULT '',
   `mail` varchar(255) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL DEFAULT 'img/users/default.png',
-  `year` int(11) NOT NULL,
-  `department` varchar(255) NOT NULL DEFAULT '',
-  `subject` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `name`, `mail`, `password`)
+VALUES
+	(1,'栗栖','ryooomaaa0413@gmail.com','$2a$10$3rCT9hO/RZu1Qxd.lhJA7.6EDUZ5SPTVcVlu4KKQZNPYin.rKNJ0S'),
+	(2,'津國健太','tsukuni@gmail.com','$2a$10$g0Xsy1kZ3eoAOxNRdXe.xOTHpw1SKMtRlOSsMgamhWqA6DYPevu3a'),
+	(3,'櫻井','sakurai@gmail.com','$2a$10$Stqc3LpJYS7.yzvF3mFIku6ZO/9Apr7KN8BvtSOLqgI6sTfxdD4UC'),
+	(4,'相田','aida@gmail.com','$2a$10$XMGMyBAaoRYHWdrzY7HfKu3Wcla6S9w3BThdYKd8NehrgYnU81aRO'),
+	(5,'admin','admin@user1234','$2a$10$Rj43eRZVwv4ISdNxCuIi0O3PC3NBNnaUNzXAk1UHnDJ5fgImoUdWO'),
+	(6,'津國健太','tamkchi.fugu@gmail.com','$2a$10$DoukLJ3RsNIua1iw2dHLLuUKXz67LOMvkJsyjYTQQssH2HHUvRTCi'),
+	(7,'しょ','keyaki.1204@docomo.ne.jp','$2a$10$Wfq.k8YlJphRhmGpSy8z0.IPaHWVwCkHijyR7nCS7BGlB/omlYsJ.'),
+	(8,'櫻井啓裕','kei.wata.taekwondo@gmail.com','$2a$10$qqyAYPvtKMQNzyaWZ2XqIubv7vRKN0p3uobC7WHHzMQbveG/QU58a'),
+	(9,'村田裕希','2018orite@gmail.com','$2a$10$mSToa6asvkzIrJ.5drZD.uUffY7ki7U/9SKho2eOTohaJLMMVMviK'),
+	(10,'<script>alert(1);</script>','a@a','$2a$10$kSzL1Ir9st00s34u7jMwBujdwtc2oqfULRc.rTvXkHkNWjhgrn2ee'),
+	(11,'Kskmdk','Sample@sample','$2a$10$sOcYZYurRcwI3e.NYL/U1eqmfDhhWCBuwPcJgXK/HtXJLpN15vW.K'),
+	(12,'` or 1=1 --','a@aaa','$2a$10$X5vAiVG6nzbobQhQFeBSFOs0CUkWHVuFkPu/fNFiXUXHNhgTkGgGu'),
+	(13,'asdfa','afadsf@asdfa','$2a$10$cMyKUHNnGpCLaLozKXDJXuwcb5ZHD9UHUoVLX7AL.8IMtkDp9ZNo6');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
