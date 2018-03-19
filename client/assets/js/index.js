ca2e31b1f2d87a4cb27db09b30adf1a45957bf8e
T_Kenta <tamkchi.fugu@gmail.com>
2018-03-19 22:00:40 +0900

    訂正

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,21 +1,15 @@
 import React  from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware,compose} from 'redux';
 import { CookiesProvider } from 'react-cookie';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 import App from './components/App';
-import persistState from 'redux-localstorage'
-
-const enhancer = compose(
-
-  persistState(),
-)
 
 let store = createStore(reducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-applyMiddleware(thunk),enhancer)
+applyMiddleware(thunk))
 render(
 		    <Provider store={store}>
 					<CookiesProvider>
